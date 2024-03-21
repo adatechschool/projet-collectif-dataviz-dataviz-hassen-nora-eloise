f2748ecd81edfc45d2702e1fdf0ba435ed02ae36
EloiseMcGlasson <mc.glasson@free.fr>
2024-03-21 09:46:34 +0100

    fonctions angles + plein de trucs

--- scripts.js
+++ scripts.js
@@ -32,14 +32,14 @@ function festivalsLoireAtlantique(){
         }
       })
     }
-    
+    //101 à 201 https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/festivals-global-festivals-_-pl/records?limit=-1&offset=101&refine=region_principale_de_deroulement%3A%22Pays%20de%20la%20Loire%22
     function festivalsMusiqueLoireAtlantique(){
         const ulElement = document.querySelector("ul")
-            fetch("https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/festivals-global-festivals-_-pl/records?limit=100&refine=discipline_dominante%3A%22Musique%22&refine=region_principale_de_deroulement%3A%22Pays%20de%20la%20Loire%22&refine=departement_principal_de_deroulement%3A%22Loire-Atlantique%22").then((response)=>{
+            fetch("https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/festivals-global-festivals-_-pl/exports/json").then((response)=>{
             return response.json()
           }).then((festivals)=>{
-            console.log(festivals.results)
-            for (const festival of festivals.results){
+            console.log(festivals)
+            for (const festival of festivals){
                const liElement=document.createElement("li")
                liElement.innerText=festival.nom_du_festival
                ulElement.appendChild(liElement)
@@ -47,5 +47,28 @@ function festivalsLoireAtlantique(){
           })
         }
     
-    
+        function map(){
+
+          window.onload = function(){
+            console.log("page chargée")
+            var map = L.map('map').setView([51.505, -0.09], 13);
+            var tileStreets = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        maxZoom: 19,
+                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>', 
+                        accessToken :'pk.eyJ1IjoiaGFzc2VuLTg1IiwiYSI6ImNsdHlqdWJ1ODBneTIybXFmZWpjcXRwbzcifQ.dug0wSKr1c_VIULUT1dSgQ'
+
+                    })
+            tileStreets.addTo(map);
+        }
+        }
+        list=[163,73,44,25,16,11]
+        function anglesCamembert(total, valeurs){
+          valeurs.forEach(element => {
+            pourcentage=(100*element)/total
+            angle = (360*pourcentage)/100
+          console.log(pourcentage, angle)
+          });
+          
+
+        }
     
