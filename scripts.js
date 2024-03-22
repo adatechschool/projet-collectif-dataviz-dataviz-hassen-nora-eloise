16303203f9c2b01a355cb48c661c41b93bd2970d
nora <noraidris621@gmail.com>
2024-03-22 15:59:57 +0100

    chart

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function festivalsPaysLoire(){
-  const ulElement = document.querySelector("ul")
+  const ulElement = document.querySelector("dd")
       fetch("https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/festivals-global-festivals-_-pl/records?limit=-1&refine=region_principale_de_deroulement%3A%22Pays%20de%20la%20Loire%22").then((response)=>{
       return response.json()
     }).then((festivals)=>{
@@ -32,7 +32,7 @@ function festivalsPaysLoire(){
       }
       
       function festivals(){
-          const ulElement = document.querySelector("ul")
+          const ulElement = document.querySelector("dd")
               fetch("https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/festivals-global-festivals-_-pl/exports/json").then((response)=>{
               return response.json()
             }).then((festivals)=>{ //festivals contient 7283 objets festival par liste de 100
@@ -51,7 +51,7 @@ function festivalsPaysLoire(){
           }
     
           
-  function carte(){
+  function map(){
     window.onload = function(){
       console.log("page chargée")
       var map = L.map('map').setView([47.22105206554747,-1.5328920498252216], 13);
